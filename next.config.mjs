e1d07a461473c6139cfdc6e3f9106f2e4031cbf7
duyanh-ct <anhleduy@chotot.vn>
2024-10-21 16:21:58 +0700

    feat: first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["www.google.com", "static.chotot.com"],
+  },
+};
 
 export default nextConfig;
