b06665fcd1b408448fbac54c43aa3ac880c8c2db
duyanh-ct <anhleduy@chotot.vn>
2025-08-14 10:28:39 +0700

    Revert "feat: first commit"
    This reverts commit e1d07a461473c6139cfdc6e3f9106f2e4031cbf7.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["www.google.com", "static.chotot.com"],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
